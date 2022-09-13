b29ae8b6fa35e3ad97c30c5b131d6d834f049f8f
andyweaves <andyweaves>
2022-09-13 18:56:55 +0000

    Additional commands

--- notebooks/workspace_debug.py
+++ notebooks/workspace_debug.py
@@ -1,4 +1,9 @@
 # Databricks notebook source
+# MAGIC %md
+# MAGIC ### Confirm that PrivateLink and connections back to the Control Plane are working
+
+# COMMAND ----------
+
 # MAGIC %sh
 # MAGIC nslookup london.cloud.databricks.com
 
@@ -16,3 +21,13 @@
 
 # MAGIC %sh
 # MAGIC nc -zv mdio2468d9025m.c6fvhwk6cqca.eu-west-2.rds.amazonaws.com 3306
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### Confirm that the Firewall is working
+
+# COMMAND ----------
+
+# MAGIC %sh
+# MAGIC curl -v --max-time 10 https://www.databricks.com
