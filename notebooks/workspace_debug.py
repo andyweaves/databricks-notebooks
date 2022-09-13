ae8a25cd9dff98a68db11c29bf27997ccd8d0a0b
andyweaves <andyweaves>
2022-09-13 18:53:22 +0000

    Some initial connectivity tests

--- notebooks/workspace_debug.py
+++ notebooks/workspace_debug.py
@@ -1,6 +1,18 @@
 # Databricks notebook source
-print("hello world!")
+# MAGIC %sh
+# MAGIC nslookup london.cloud.databricks.com
 
 # COMMAND ----------
 
+# MAGIC %sh
+# MAGIC curl -v -L https://london.cloud.databricks.com
 
+# COMMAND ----------
+
+# MAGIC %sh
+# MAGIC nslookup tunnel.eu-west-2.cloud.databricks.com
+
+# COMMAND ----------
+
+# MAGIC %sh
+# MAGIC nc -zv mdio2468d9025m.c6fvhwk6cqca.eu-west-2.rds.amazonaws.com 3306
