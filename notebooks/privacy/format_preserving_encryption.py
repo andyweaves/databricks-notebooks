9dfa42a7d4b4457ba7a1b19aed1f1df8747ee989
andrew.weaver@databricks.com <andrew.weaver@databricks.com>
2023-05-19 18:45:52 +0000

    Both pandas and python UDF working

--- notebooks/privacy/format_preserving_encryption.py
+++ notebooks/privacy/format_preserving_encryption.py
@@ -14,72 +14,78 @@
 
 # COMMAND ----------
 
+# Generate some fake PII for us to FPE...
 df = generate_fake_pii_data(num_rows=1000).select("customer_id", "name", "email", "ssn", "iban", "credit_card", "phone_number", "date_of_birth", "ipv4", "ipv6")
-display(df)
-
-# COMMAND ----------
-
 df.write.mode("overwrite").saveAsTable("diz.raw.fake_pii_data")
+display(df)
 
 # COMMAND ----------
 
 import secrets  
   
-# Generate a 256 bit key
+# If needed generate a 256 bit key, store as a secret...
 key = secrets.token_bytes(32).hex()
 
-# Generate a 7 byte tweak
+# If needed generate a 256 bit key, store as a secret...
 tweak = secrets.token_bytes(7).hex()
 
+# Retrieve the key & tweak...
 key = dbutils.secrets.get("aweaver", "fpe_key")
 tweak = dbutils.secrets.get("aweaver", "fpe_tweak")
 
+# Determine what to do with any special characters in the plaintext...
+#   Options are "tokenize", "strip", or "reassemble" where:
+# 
+#     1. "tokenize" -> 
+#     2. "strip" -> 
+#     3. "reassemble" ->  
+#
+special_char_mode="reassemble" 
+
+# Define character sets...
+numeric_charset = "0123456789"
+alpha_charset_upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+alpha_charset_lower = "abcdefghijklmnopqrstuvwxyz"
+alpa_charset_all = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+alpanumeric_charset = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+ascii_charset = """0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~ \t\n\r\x0b\x0c"""
+special_charset = """!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~ """
+
 # COMMAND ----------
 
-def fpe_encrypt(key, tweak, special_char_mode, plaintext):
+from ff3 import FF3Cipher
 
-  ciphertext = None
-
-  numeric_charset = "0123456789"
-  alpha_charset_upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-  alpha_charset_lower = "abcdefghijklmnopqrstuvwxyz"
-  alpa_charset_all = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-  alpanumeric_charset = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-  ascii_charset = """0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~ \t\n\r\x0b\x0c"""
-  special_charset = """!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~ """
-
-  if not isinstance(plaintext, str):
-    raise TypeError(f"Only strings can be FPE encrypted, but received {type(plaintext)}. Please cast to string.")
-
-  from ff3 import FF3Cipher
-
-  def encrypt(plaintext, charset):
-
-    c = FF3Cipher(key, tweak, radix=10).withCustomAlphabet(key, tweak, charset)
-
-    if len(plaintext) > 28:
-      split_string = lambda string: (lambda s: s[:-1] + [s[-2] + s[-1]] if len(s[-1]) < 4 else s)([string[i:i+23] for i in range(0, len(string), 23)])  
-      split = split_string(plaintext)
-      ciphertext = "".join(list(map(lambda x: c.encrypt(x), split)))
-      return ciphertext
-    else: 
-      return c.encrypt(plaintext)
-
-  def reassemble_string(string, positions, characters):
-    for i in range(len(positions)):  
-      pos = positions[i]   
-      char = characters[i]  
-      string = string[:pos] + char + string[pos:]
-    return string
+def reassemble_string(string: str, positions: list, characters: str) -> str:
+  for i in range(len(positions)):  
+    pos = positions[i]   
+    char = characters[i]  
+    string = string[:pos] + char + string[pos:]
+  return string
   
-  def encrypt_alpha(plaintext):
-    if plaintext.isupper():
-      ciphertext = encrypt(plaintext, alpa_charset_all) 
-    elif plaintext.islower():
-      ciphertext = encrypt(plaintext, alpa_charset_all) 
-    else:  
-      ciphertext = encrypt(plaintext, alpa_charset_all) 
-    return ciphertext 
+def encrypt_alpha(plaintext: str) -> str:
+  if plaintext.isupper():
+    ciphertext = encrypt(plaintext, alpha_charset_upper) 
+  elif plaintext.islower():
+    ciphertext = encrypt(plaintext, alpha_charset_lower) 
+  else:  
+    ciphertext = encrypt(plaintext, alpa_charset_all) 
+  return ciphertext 
+
+def encrypt(plaintext: str, charset: str) -> str:
+
+  c = FF3Cipher.withCustomAlphabet(key, tweak, charset)
+
+  if len(plaintext) > 28:
+    split_string = lambda string: (lambda s: s[:-1] + [s[-2] + s[-1]] if len(s[-1]) < 4 else s)([string[i:i+23] for i in range(0, len(string), 23)])  
+    split = split_string(plaintext)
+    ciphertext = "".join(list(map(lambda x: c.encrypt(x), split)))
+    return ciphertext
+  else: 
+    return c.encrypt(plaintext)
+
+# COMMAND ----------
+
+def fpe_encrypt(plaintext: str) -> str:
 
   if plaintext.isnumeric():
     ciphertext = encrypt(plaintext, numeric_charset)
@@ -117,7 +123,7 @@ def fpe_encrypt(key, tweak, special_char_mode, plaintext):
 from pyspark.sql.functions import udf
 from pyspark.sql.types import StringType
 
-fpe_encrypt_udf = udf(lambda x: fpe_encrypt(key=key, tweak=tweak, special_char_mode="reassemble", plaintext=x), StringType())
+fpe_encrypt_udf = udf(lambda x: fpe_encrypt(str(x)), StringType())
 
 # COMMAND ----------
 
@@ -148,26 +154,40 @@ display(tokenized)
 
 # COMMAND ----------
 
-import pandas as pd
-from pyspark.sql.functions import col, pandas_udf
-from pyspark.sql.types import LongType
+from pyspark.sql.functions import pandas_udf
 
-# Declare the function and create the UDF
-def multiply_func(a: pd.Series, b: pd.Series) -> pd.Series:
-    return a * b
+def fpe_encrypt_series(s: pd.Series) -> pd.Series:
 
-multiply = pandas_udf(multiply_func, returnType=LongType())
+  return s.astype(str).apply(lambda x: fpe_encrypt(x))
 
-# The function for a pandas_udf should be able to execute with local pandas data
-x = pd.Series([1, 2, 3])
-print(multiply_func(x, x))
-# 0    1
-# 1    4
-# 2    9
-# dtype: int64
+fpe_encrypt_pandas_udf = pandas_udf(fpe_encrypt_series, returnType=StringType())
 
-# Create a Spark DataFrame, 'spark' is an existing SparkSession
-df = spark.createDataFrame(pd.DataFrame(x, columns=["x"]))
+# COMMAND ----------
 
-# Execute function as a Spark vectorized UDF
-df.select(multiply(col("x"), col("x"))).show()
+x = pd.Series(["809-81-8331", "GB08TZDS66404627722762", "340429565139169"])
+
+print(fpe_encrypt_series(x))
+
+# COMMAND ----------
+
+tokenized = (spark.table("diz.raw.fake_pii_data")
+  .select(
+    "customer_id",
+    "name",
+    fpe_encrypt_pandas_udf(col("name")).alias("tokenized_name"),
+    "date_of_birth",
+    fpe_encrypt_pandas_udf(col("date_of_birth").cast("string")).alias("tokenized_date_of_birth"),
+    "ssn",
+    fpe_encrypt_pandas_udf(col("ssn")).alias("tokenized_ssn"),
+    "iban",
+    fpe_encrypt_pandas_udf(col("iban")).alias("tokenized_iban"),
+    "credit_card",
+    fpe_encrypt_pandas_udf(col("credit_card").cast("string")).alias("tokenized_credit_card"),
+    "phone_number",
+    fpe_encrypt_pandas_udf(col("phone_number")).alias("tokenized_phone_number"),
+    "ipv4",
+    fpe_encrypt_pandas_udf(col("ipv4")).alias("tokenized_ipv4"),
+    "ipv6",
+    fpe_encrypt_pandas_udf(col("ipv6")).alias("tokenized_ipv6")
+  ))
+display(tokenized)
