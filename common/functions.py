6c661fd2b3a56ab7303668812f9b5335b98e371d
andyweaves <andyweaves>
2023-08-07 19:04:42 +0000

    Major refactor for better modularisation

--- common/functions.py
+++ common/functions.py
@@ -78,3 +78,193 @@ def generate_fake_pii_data(num_rows=1000):
   .groupBy("partition_id")
   .applyInPandas(generate_fake_data, schema)
   .orderBy(asc("customer_id")))
+
+# COMMAND ----------
+
+def get_selection(selection: list, all_options: list) -> list:
+
+  if "ALL" in selection:
+    return all_options
+  else:
+    return selection
+
+# COMMAND ----------
+
+# See https://microsoft.github.io/presidio/supported_entities/ 
+
+all_supported_entities = ["CREDIT_CARD", "CRYPTO", "DATE_TIME", "EMAIL_ADDRESS", "IBAN_CODE", "IP_ADDRESS", "NRP", "LOCATION", "PERSON", "PHONE_NUMBER", "MEDICAL_LICENSE", "URL", "US_BANK_NUMBER", "US_DRIVER_LICENSE", "US_ITIN", "US_PASSPORT", "US_SSN", "UK_NHS", "ES_NIF", "IT_FISCAL_CODE", "IT_DRIVER_LICENSE", "IT_VAT_CODE", "IT_PASSPORT", "IT_IDENTITY_CARD", "SG_NRIC_FIN", "AU_ABN", "AU_ACN", "AU_TFN", "AU_MEDICARE"]
+
+# COMMAND ----------
+
+from pyspark.sql import SparkSession, DataFrame
+from pyspark.broadcast import Broadcast
+from pyspark.sql.functions import asc, col, when, lit, from_json, explode, mean, count, pandas_udf
+from pyspark.sql.types import StringType, ArrayType, StructType, StructField, IntegerType, DoubleType
+import pandas as pd
+import json
+from datetime import date
+
+class PIIScanner:
+
+  def __init__(
+    self, 
+    spark: SparkSession, broadcasted_analyzer: Broadcast, entities: list, 
+    language: str = "en",  sample_size: int = 1000, average_score: float = 0.5, hit_rate: int = 60):
+      self.spark = spark 
+      self.broadcasted_analyzer = broadcasted_analyzer
+      self.entities = entities
+      self.language = language
+      self.sample_size = sample_size
+      self.average_score = average_score
+      self.hit_rate = hit_rate
+      self.scan_schema = ArrayType(StructType([
+        StructField("entity_type", StringType()),
+        StructField("start", IntegerType()),
+        StructField("end", IntegerType()),
+        StructField("score", DoubleType())
+      ]))
+      self.results_schema = StructType([
+        StructField("column", StringType()),
+        StructField("entity_type", StringType()),
+        StructField("num_entities", DoubleType()),
+        StructField("avg_score", DoubleType()),
+        StructField("sample_size", IntegerType()),
+        StructField("hit_rate", DoubleType()),
+    ])
+      print(f"PII Scanner initialized using language {self.language.upper()}. Looking for entities {self.entities} with a sample size of {self.sample_size}, an average score of {self.average_score} and a hit rate of {self.hit_rate}.")
+
+  @staticmethod
+  def get_all_uc_tables(spark: SparkSession, catalogs: list) -> DataFrame:
+
+    print(f"Getting all uc tables in catalogs {catalogs}")
+  
+    return (
+      spark.sql(f"SELECT * FROM system.information_schema.tables WHERE table_catalog IN {catalogs}")
+      .select("table_catalog", 
+              "table_schema", 
+              "table_name",
+              when(col("table_type") == "VIEW", "VIEW").otherwise(lit("TABLE")).alias("table_type"),
+              "created", 
+              "last_altered").
+      orderBy(
+        col("table_catalog").asc(), 
+        col("table_schema").asc(), 
+        col("table_name").asc()))
+
+  def _get_aggregated_results(self, df: DataFrame) -> DataFrame:
+
+    df_size = df.count()
+
+    if df_size != self.sample_size:
+      sample_size = df_size
+    else:
+      sample_size = self.sample_size
+    results_df = spark.createDataFrame([], self.results_schema)
+
+    for c in df.columns:
+      
+      exploded = df.select(lit(c).alias("column"), explode(col(c)).alias(c))
+      new_df = (exploded.select(
+        col("column"),
+        col(f"{c}.entity_type"), 
+        col(f"{c}.score")).groupBy("column", "entity_type")
+                .agg(
+                  count("entity_type").alias("num_entities"),
+                  mean("score").alias("avg_score"))
+                .withColumn("sample_size", lit(sample_size))
+                .withColumn("hit_rate", col("num_entities") / col("sample_size") * 100)
+                .where(f"hit_rate >= {self.hit_rate} AND avg_score >= {self.average_score}"))
+      results_df = results_df.union(new_df)
+
+    return results_df
+
+  def _scan_dataframe(self, df: DataFrame) -> DataFrame:
+    return (df.select([from_json(analyze_udf(col(c).cast("string")), self.scan_schema).alias(c) for c in df.columns]))
+
+  def scan_dataframe(self, df: DataFrame) -> DataFrame:
+
+    try:
+      scanned = self._scan_dataframe(df).limit(self.sample_size)
+      results = self._get_aggregated_results(scanned)
+
+    except Exception as e:
+      print(f"Failed to scan {securable_type} {securable_namespace} because of exception {e}")
+
+    return results
+  
+  def add_tag(self, securable_namespace: str, securable_type: str, tag: str) -> None:
+
+    try:
+      print(f"Adding tag '{tag}' to {securable_type} {securable_namespace}")
+      sql(f"ALTER {securable_type} {securable_namespace} SET TAGS ('{tag}')")
+    
+    except Exception as e:
+      print(f"Failed to add tag '{tag}' to {securable_type} {securable_namespace} because of exception {e}")
+
+  def add_column_tag(self, securable_namespace: str, securable_type: str, column: str, tag: str) -> None:
+
+    try:
+      print(f"Adding tag '{tag}' to column '{column}' on {securable_type} {securable_namespace}")
+      sql(f"ALTER {securable_type} {securable_namespace} ALTER COLUMN SET '{column}' TAGS ('{tag}')")
+    
+    except Exception as e:
+      print(f"Failed to add tag '{tag}' to column '{column}' on {securable_type} {securable_namespace} because of exception {e}")
+
+  def add_comment(self, securable_namespace: str, securable_type: str, comment: str) -> None:
+
+    try:
+      print(f"Adding comment '{comment}' to {securable_type} {securable_namespace}")
+      sql(f"COMMENT ON {securable_type} {securable_namespace} IS '{comment}'")
+    
+    except Exception as e:
+      print(f"Failed to add comment '{comment}' to {securable_type} {securable_namespace} because of exception {e}")
+
+  def add_column_comment(self, securable_namespace: str, securable_type: str, column: str, comment: str) -> None:
+
+    try:
+      print(f"Adding comment '{comment}' to column '{column}' on {securable_type} {securable_namespace}")
+      sql(f"ALTER {securable_type} {securable_namespace} ALTER COLUMN '{column}' COMMENT '{comment}'")
+    
+    except Exception as e:
+      print(f"Failed to add comment '{comment}' to column '{column}' on {securable_type} {securable_namespace} because of exception {e}")
+  
+  def _get_table_comment(self, date: datetime.date) -> str:
+
+    return f"""
+> # `WARNING! This table contains PII`
+# Table Scanned on `{date}`"""
+
+  def _get_column_comment(self, column_json) -> str:
+
+    return f"""---
+  > ## `WARNING! This column contains PII`
+  ```
+  {json.dumps(column_json)}
+  ```
+  """
+
+  def scan_and_tag_securable(self, securable_namespace: str, securable_type: str) -> DataFrame:
+
+    today = date.today()
+    print(f"Scanning {securable_type} {securable_namespace} for PII.")
+
+    df = self.spark.table(securable_namespace).limit(self.sample_size)
+    scanned = self._scan_dataframe(df)
+    results = self._get_aggregated_results(scanned).toPandas()
+
+    if len(results) > 0:
+      self.add_tag(securable_namespace=securable_namespace, securable_type=securable_type, tag='PII')
+      if securable_type == "TABLE":
+        self.add_comment(securable_namespace=securable_namespace, securable_type=securable_type, comment=self._get_table_comment(today))
+      for index, value in results["column"].drop_duplicates().items():
+        result = results[results["column"] == value] 
+        column_json = []
+        for index, row in result.iterrows():
+          self.add_tag(securable_namespace, securable_type, row.entity_type)
+          column_json.append(row.to_json(indent=2))
+        if securable_type == "TABLE":
+          self.add_column_tag(securable_namespace=securable_namespace, securable_type=securable_type, column=row.column, tag=row.entity_type)
+          self.add_column_comment(self, securable_namespace=securable_namespace, securable_type=securable_type, column=row.column, comment=self._get_column_comment(column_json))
+      results.insert(0, "scan_date", today)
+      results.insert(1, "securable", securable_namespace)
+    return results
--- notebooks/privacy/identifying_and_tagging_pii.py
+++ notebooks/privacy/identifying_and_tagging_pii.py
@@ -7,99 +7,42 @@
 
 # COMMAND ----------
 
-all_catalogs = list(filter(None, [x[0] for x in sql("SHOW CATALOGS").collect()]))
+all_catalogs = list(filter(None, [x[0] for x in sql("SHOW CATALOGS").limit(1000).collect()]))
 catalogs = all_catalogs.copy()
 catalogs.insert(0, "ALL")
 
-# See https://microsoft.github.io/presidio/supported_entities/ 
-all_supported_entities = ["CREDIT_CARD", "CRYPTO", "DATE_TIME", "EMAIL_ADDRESS", "IBAN_CODE", "IP_ADDRESS", "NRP", "LOCATION", "PERSON", "PHONE_NUMBER", "MEDICAL_LICENSE", "URL", "US_BANK_NUMBER", "US_DRIVER_LICENSE", "US_ITIN", "US_PASSPORT", "US_SSN", "UK_NHS", "ES_NIF", "IT_FISCAL_CODE", "IT_DRIVER_LICENSE", "IT_VAT_CODE", "IT_PASSPORT", "IT_IDENTITY_CARD", "SG_NRIC_FIN", "AU_ABN", "AU_ACN", "AU_TFN", "AU_MEDICARE"]
 supported_entities = all_supported_entities.copy()
 supported_entities.insert(0, "ALL")
 
 dbutils.widgets.multiselect(name="catalogs", defaultValue="ALL", choices=catalogs, label="catalogs_to_scan")
 dbutils.widgets.multiselect(name="entities", defaultValue="ALL", choices=supported_entities, label="entities_to_detect")
-dbutils.widgets.dropdown(name="sample_size", defaultValue="1000", choices=["100", "1000", "10000"], label="sample_size")
-dbutils.widgets.dropdown(name="hit_rate", defaultValue="60", choices=["50", "60", "70", "80", "90"], label="hit_rate")
-dbutils.widgets.dropdown(name="average_score", defaultValue="0.5", choices=["0.5", "0.6", "0.7", "0.8", "0.9"], label="average_score")
-# To change the language you will need to download the relevant spacy model. See https://microsoft.github.io/presidio/analyzer/languages/
-dbutils.widgets.dropdown(name="language", defaultValue="en", choices=["en"], label="language")
-
-def get_selection(selection, all_options):
-
-  if "ALL" in selection:
-    return all_options
-  else:
-    return selection
-
-# COMMAND ----------
-
-CATALOGS = tuple(get_selection(selection=dbutils.widgets.get("catalogs").split(","), all_options=all_catalogs))
-ENTITIES = get_selection(selection=dbutils.widgets.get("entities").split(","), all_options=all_supported_entities)
-SAMPLE_SIZE = int(dbutils.widgets.get("sample_size"))
-
-# To change the language you will need to download the relevant spacy model. See https://microsoft.github.io/presidio/analyzer/languages/
-LANGUAGE = dbutils.widgets.get("language")
-
-# Reduce false positives by narrowing down the list of entities and introducing a threshold on the confidence score...
-HIT_RATE = int(dbutils.widgets.get("hit_rate"))
-AVG_SCORE = float(dbutils.widgets.get("average_score"))
-
-print(f"Scanning catalogs {CATALOGS} for PII entities {ENTITIES} using language {LANGUAGE} with a sample size of {SAMPLE_SIZE}. Filtering results based on a hit rate of {HIT_RATE} and an average score of {AVG_SCORE}...")
-
-# COMMAND ----------
-
-from pyspark.sql.functions import asc, col, when, lit
-
-TABLES = (
-  spark.sql(f"SELECT * FROM system.information_schema.tables WHERE table_catalog IN {CATALOGS}")
-  .select("table_catalog", "table_schema", "table_name", when(col("table_type") == "VIEW", "VIEW").otherwise(lit("TABLE")).alias("table_type"), "created", "last_altered")
-  .orderBy(col("table_catalog").asc(), col("table_schema").asc(), col("table_name").asc()))
-
-display(TABLES)
+  
+catalogs = tuple(get_selection(selection=dbutils.widgets.get("catalogs").split(","), all_options=all_catalogs))
+entities = get_selection(selection=dbutils.widgets.get("entities").split(","), all_options=all_supported_entities)
 
 # COMMAND ----------
 
 from presidio_analyzer import AnalyzerEngine
-from pyspark.sql.functions import pandas_udf
-from pyspark.sql.types import StringType
-import pandas as pd
-import json
 
-analyzer = AnalyzerEngine()
+broadcasted_analyzer = sc.broadcast(AnalyzerEngine())
 
-# broadcast the engines to the cluster nodes
-broadcasted_analyzer = sc.broadcast(analyzer)
-
-# define a pandas UDF function and a series function over it.
+# In an ideal world we would define the UDFs in the class, but a Spark UDF can only be defined in a class as a static method...
 def analyze_text(text: str) -> str:
     analyzer = broadcasted_analyzer.value
-    analyzer_results = analyzer.analyze(text=text, language=LANGUAGE, entities=ENTITIES)
+    analyzer_results = analyzer.analyze(text=text, entities=entities, language="en")
     return json.dumps([x.to_dict() for x in analyzer_results]) 
 
 def analyze_series(s: pd.Series) -> pd.Series:
     return s.astype(str).apply(analyze_text)
 
-# define the function as pandas UDF
-analyze = pandas_udf(analyze_series, returnType=StringType())
+analyze_udf = pandas_udf(analyze_series, returnType=StringType())
+
+pii_scanner = PIIScanner(spark=spark, broadcasted_analyzer=broadcasted_analyzer, entities=entities, language="en")
 
 # COMMAND ----------
 
-from pyspark.sql.types import ArrayType, StructType, StructField, IntegerType, DoubleType
-from pyspark.sql.functions import from_json, transform
-
-scan_schema = ArrayType(
-  StructType([
-    StructField("entity_type", StringType()),
-    StructField("start", IntegerType()),
-    StructField("end", IntegerType()),
-    StructField("score", DoubleType())
-  ]))
-
-def scan_dataframe(input_df):
-
-  input_df = input_df.limit(SAMPLE_SIZE).select([from_json(analyze(col(c).cast("string")), scan_schema).alias(c) for c in input_df.columns])
-
-  return input_df
+all_tables = pii_scanner.get_all_uc_tables(spark=spark, catalogs=catalogs).where("table_schema != 'information_schema'")
+display(all_tables)
 
 # COMMAND ----------
 
@@ -108,139 +51,28 @@ display(df)
 
 # COMMAND ----------
 
-test_scan = df.transform(scan_dataframe)
+test_scan = df.transform(pii_scanner.scan_dataframe)
 display(test_scan)
 
 # COMMAND ----------
 
-from pyspark.sql.functions import explode, lit, mean, count
+import os
+import concurrent.futures
 
-results_schema = StructType([
-    StructField("column", StringType()),
-    StructField("entity_type", StringType()),
-    StructField("num_entities", DoubleType()),
-    StructField("avg_score", DoubleType()),
-    StructField("sample_size", IntegerType()),
-    StructField("hit_rate", DoubleType()),
-  ])
+scan_results = pd.DataFrame()
 
-def get_aggregated_results(df):
+with concurrent.futures.ThreadPoolExecutor(max_workers=os.cpu_count()) as executor:
 
-  df_size = df.count()
+  futures = [executor.submit(pii_scanner.scan_and_tag_securable, f"{securable.table_catalog}.{securable.table_schema}.{securable.table_name}", securable.table_type) for securable in all_tables.collect()]
+  
+  for future in concurrent.futures.as_completed(futures):
 
-  if df_size != SAMPLE_SIZE:
-    sample_size = df_size
-  else:
-    sample_size = SAMPLE_SIZE
-
-  agg_df = spark.createDataFrame([], results_schema)
-
-  for c in df.columns:
-
-    exploded = df.select(lit(c).alias("column"), explode(col(c)).alias(c))
-
-    new_df = (exploded.select(
-      col("column"),
-      col(f"{c}.entity_type"), 
-      col(f"{c}.score")).groupBy("column", "entity_type")
-    .agg(
-      count("entity_type").alias("num_entities"),
-      mean("score").alias("avg_score"))
-    .withColumn("sample_size", lit(sample_size))
-    .withColumn("hit_rate", col("num_entities") / col("sample_size") * 100)
-    .where(f"hit_rate >= {HIT_RATE} AND avg_score >= {AVG_SCORE}"))
-
-    agg_df = agg_df.union(new_df)
-    
-  return agg_df
+    result = future.result()
+    if isinstance(result, pd.DataFrame):
+      scan_results = pd.concat([scan_results, result])
+    else:
+      print(result)
 
 # COMMAND ----------
 
-test_aggregated_results = test_scan.transform(get_aggregated_results)
-display(test_aggregated_results)
-
-# COMMAND ----------
-
-from datetime import datetime
-
-scanned_tables, tagged_tables, unscanned_tables, untagged_tables = [], [], [], []
-today = datetime.today().strftime("%d/%m/%Y")
-tables = TABLES.collect()
-
-for t in tables:
-
-  try:
-
-    print(f"Scanning {t.table_catalog}.{t.table_schema}.{t.table_name} for PII")
-    scanned = spark.table(f"{t.table_catalog}.{t.table_schema}.{t.table_name}").transform(scan_dataframe)
-    aggregated_results = scanned.transform(get_aggregated_results).toPandas()
-    scanned_tables.append(t)
-
-    if len(aggregated_results) > 0:
-
-      try:
-        print(f"Adding PII tags to {t.table_catalog}.{t.table_schema}.{t.table_name}")
-        sql(f"ALTER {t.table_type} {t.table_catalog}.{t.table_schema}.{t.table_name} SET TAGS ('PII')")
-
-        if t.table_type == "TABLE":
-
-          table_comment = f"""
-> # `WARNING! This table contains PII`
-> Table Scanned on `{today}`"""
-          
-          sql(f"COMMENT ON {t.table_type} {t.table_catalog}.{t.table_schema}.{t.table_name} IS '{table_comment}'")
-        
-        for index, value in aggregated_results["column"].drop_duplicates().items():
-
-          result = aggregated_results[aggregated_results["column"] == value] 
-          column_json = []
-
-          for index, row in result.iterrows():
-            
-            sql(f"ALTER {t.table_type} {t.table_catalog}.{t.table_schema}.{t.table_name} SET TAGS ('{row.entity_type}')")
-            column_json.append(row.to_json(indent=2))
-            
-          column_comment = f"""
----
-### `WARNING! This column contains PII`
-```
-{json.dumps(column_json)}
-```
-"""
-
-          if t.table_type == "TABLE":
-
-            sql(f"ALTER {t.table_type} {t.table_catalog}.{t.table_schema}.{t.table_name} ALTER COLUMN {row.column} SET TAGS ('{row.entity_type}')")
-            sql(f"ALTER {t.table_type} {t.table_catalog}.{t.table_schema}.{t.table_name} ALTER COLUMN {row.column} COMMENT '{column_comment}'")
-        tagged_tables.append(t)
-
-      except Exception as e:
-        print(f"Unable to add PII tags to {t.table_catalog}.{t.table_schema}.{t.table_name} due to exception {e}")
-        untagged_tables.append(t)
-
-  except Exception as e:
-
-    print(f"Unable to scan {t.table_catalog}.{t.table_schema}.{t.table_name} for PII due to exception {e}")
-    unscanned_tables.append(t)
-
-for t in unscanned_tables:
-
-  try:
-
-    print(f"Adding tag 'NOT_PII_SCANNED' to {t.table_catalog}.{t.table_schema}.{t.table_name}")
-    if t.table_type == "TABLE":
-      sql(f"ALTER {t.table_type} {t.table_catalog}.{t.table_schema}.{t.table_name} SET TAGS ('NOT_PII_SCANNED')")
-
-  except Exception as e:
-
-    print(f"Unable to add NOT_PII_SCANNED tags to {t.table_catalog}.{t.table_schema}.{t.table_name} due to exception {e}")
-    untagged_tables.append(t)  
-
-# COMMAND ----------
-
-print(f"""
-Scanned {len(scanned_tables)} of {len(tables)} tables for PII. 
-Tagged {len(tagged_tables)} tables because they contained PII. 
-Unable to scan {len(unscanned_tables)} tables.
-Unable to tag {len(untagged_tables)} tables.
-""")
+display(scan_results.sort_values(by=["securable"]))
