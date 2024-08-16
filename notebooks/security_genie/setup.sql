063b5ec27547c9bc1171456a63d683d2a8f8a2b3
andrew.weaver@databricks.com <andrew.weaver@databricks.com>
2024-08-16 11:45:47 +0000

    Changes

--- notebooks/security_genie/setup.sql
+++ notebooks/security_genie/setup.sql
@@ -8,17 +8,26 @@ USE SCHEMA IDENTIFIER(:schema)
 
 -- COMMAND ----------
 
+USE CATALOG ${catalog};
+USE SCHEMA IDENTIFIER(:schema)
+
+-- COMMAND ----------
+
 CREATE
 OR REPLACE FUNCTION failed_authentication_attempts()
 RETURNS TABLE
-COMMENT 'Can be used to detect repeated failed authentication attempts, which could indicate an attacker trying to brute force access to your account'
+COMMENT 'Can be used to detect repeated failed authentication attempts, which could indicate an attacker trying to brute force access to your account. The results are limited to the last 90 days.'
 RETURN
   SELECT
   event_date,
+  'AUTHN_FAILED' AS event_type,
   ifnull(user_identity.email, request_params.user) AS username,
+  count(*) AS total_failed_attempts,
+  count(distinct workspace_id) AS number_of_different_workspaces,
   count(distinct source_ip_address) AS number_of_different_ips,
+  count_if(audit_level = 'ACCOUNT_LEVEL') AS number_of_account_level_failures,
+  count_if(audit_level = 'WORKSPACE_LEVEL') AS number_of_workspace_level_failures,
   count(distinct service_name) AS number_of_different_services,
-  count(distinct user_agent) AS number_of_different_user_agents,
   array_sort(collect_set(named_struct(
       'event_time', event_time,
       'source_ip_address', source_ip_address,
@@ -30,27 +39,35 @@ RETURN
       'error_message', response.error_message,
       'status_code', response.status_code
       )), (left, right) -> CASE WHEN left.event_time > right.event_time THEN -1 WHEN left.event_time < right.event_time THEN 1 ELSE 0 END)
-   AS failed_attempts,
-  count(*) AS total_failed_attempts 
+   AS failed_attempts
   FROM audit_logs 
   WHERE (contains(lower(action_name), 'login') OR contains(lower(action_name), 'auth'))  
   AND response.status_code = 401
+  --AND event_date >= (SELECT MAX(event_date) FROM audit_logs) - INTERVAL 90 DAYS
   GROUP BY ALL
   ORDER BY event_date DESC, total_failed_attempts DESC
+
+-- COMMAND ----------
+
+SELECT MAX(event_date) FROM audit_logs - INTERVAL 90 DAYS
 
 -- COMMAND ----------
 
 CREATE
 OR REPLACE FUNCTION failed_ip_access_attempts()
 RETURNS TABLE
-COMMENT 'Can be used to detect repeated failed IP access list attempts, which indicates that someone is trying to access your account from an untrusted IP address range'
+COMMENT 'Can be used to detect repeated failed IP access list attempts, which indicates that someone is trying to access your account from an untrusted IP address range. The results are limited to the last 90 days.'
 RETURN
    SELECT
   event_date,
+  'IP_ACCESS_FAILED' AS event_type,
   source_ip_address,
+  count(*) AS total_failed_attempts,
+  count(distinct workspace_id) AS number_of_different_workspaces,
   count(distinct ifnull(user_identity.email, request_params.user)) AS number_of_different_users,
+  count_if(audit_level = 'ACCOUNT_LEVEL') AS number_of_account_level_failures,
+  count_if(audit_level = 'WORKSPACE_LEVEL') AS number_of_workspace_level_failures,
   count(distinct service_name) AS number_of_different_services,
-  count(distinct user_agent) AS number_of_different_user_agents,
   array_sort(collect_set(named_struct(
       'event_time', event_time,
       'username', ifnull(user_identity.email, request_params.user),
@@ -62,16 +79,49 @@ RETURN
       'error_message', response.error_message,
       'status_code', response.status_code
       )), (left, right) -> CASE WHEN left.event_time > right.event_time THEN -1 WHEN left.event_time < right.event_time THEN 1 ELSE 0 END)
-   AS failed_attempts,
-  count(*) AS total_failed_attempts
+   AS failed_attempts
   FROM audit_logs 
-  WHERE action_name IN ('IpAccessDenied', 'accountIpAclsValidationFailed')  
+  WHERE action_name IN ('IpAccessDenied', 'accountIpAclsValidationFailed') 
+  --AND event_date >= (SELECT MAX(event_date) FROM audit_logs - INTERVAL 90 DAYS) 
   GROUP BY ALL
   ORDER BY event_date DESC, total_failed_attempts DESC
 
 -- COMMAND ----------
 
-SELECT * FROM failed_ip_access_attempts()
+CREATE
+OR REPLACE FUNCTION failed_authorisation_attempts()
+RETURNS TABLE
+COMMENT 'Can be used to detect repeated failed authorisation attempts, which could indicate attempts at lateral movement and/or privilege escalation. Note that failed IP ACL attempts are not included in this list. Use the function failed_ip_access_attempts() to investigate these instead. The results are limited to the last 90 days.'
+RETURN
+  SELECT
+  event_date,
+  'AUTHZ_FAILED' AS event_type,
+  ifnull(user_identity.email, request_params.user) AS username,
+  count(*) AS total_failed_attempts,
+  count(distinct workspace_id) AS number_of_different_workspaces,
+  count(distinct source_ip_address) AS number_of_different_ips,
+  count_if(audit_level = 'ACCOUNT_LEVEL') AS number_of_account_level_failures,
+  count_if(audit_level = 'WORKSPACE_LEVEL') AS number_of_workspace_level_failures,
+  count(distinct service_name) AS number_of_different_services,
+  count(distinct action_name) AS number_of_different_actions,
+  array_sort(collect_set(named_struct(
+      'event_time', event_time,
+      'source_ip_address', source_ip_address,
+      'user_agent', user_agent,
+      'audit_level', audit_level,
+      'workspace_id', workspace_id,
+      'service_name', service_name,
+      'action_name', action_name,
+      'error_message', response.error_message,
+      'status_code', response.status_code
+      )), (left, right) -> CASE WHEN left.event_time > right.event_time THEN -1 WHEN left.event_time < right.event_time THEN 1 ELSE 0 END)
+   AS failed_attempts
+  FROM audit_logs 
+  WHERE action_name NOT IN ('IpAccessDenied', 'accountIpAclsValidationFailed')  
+  AND response.status_code = 403
+  --AND event_date >= (SELECT MAX(event_date) FROM audit_logs) - INTERVAL 90 DAYS
+  GROUP BY ALL
+  ORDER BY event_date DESC, total_failed_attempts DESC
 
 -- COMMAND ----------
 
@@ -79,10 +129,4 @@ SELECT * FROM failed_authentication_attempts()
 
 -- COMMAND ----------
 
-SELECT 
-service_name,
-action_name,
-count(*) AS total
-FROM `system`.access.audit WHERE response.status_code = 403
-GROUP BY ALL
-ORDER BY TOTAL DESC
+SELECT * FROM failed_authorisation_attempts()
